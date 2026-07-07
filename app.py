4b2c4829ffe42566b1486a21e03d38307198550f
tanveer-deol <tanudeol90@gmail.com>
2026-07-07 17:49:19 +0530

    updated app.js

--- app.py
+++ app.py
@@ -103,5 +103,10 @@ def predict_api():
     })
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
